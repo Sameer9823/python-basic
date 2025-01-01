600d422628c903be38399975641da017d8b87458
Sameer Selokar <sameerselokar924@gmail.com>
2025-01-01 22:29:23 +0530

    Add hello.py and a.py with Chai class and example usage

---
+++ Basic/hello.py
@@ -0,0 +1,16 @@
+# hello.py
+print("hello world")
+
+class Chai:
+    chai_one = "jkngt"
+    chai_two = "kbrni"
+    chai_three = "orjoteiy"
+
+    @staticmethod
+    def make_chai(n):
+        print(n)
+
+chai = Chai()
+
+# Example usage
+chai.make_chai("lemon tea")
