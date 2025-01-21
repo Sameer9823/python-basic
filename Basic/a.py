85e6bbc63137c7111bbb1a95276002271656181e
Sameer Selokar <sameerselokar924@gmail.com>
2025-01-21 22:18:39 +0530

    Add recursive factorial function example to a.py

--- Basic/a.py
+++ Basic/a.py
@@ -6,3 +6,14 @@
 a = 29
 for i in range(9):
     print(i)
+    
+# use recursion example
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+    
+    
+print(factorial(5))
+
